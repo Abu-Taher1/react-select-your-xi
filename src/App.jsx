5745f7ef0f9137a644a0bbccb478b47757ac454b
Abu-Ther <rafi.xq.1i@gmail.com>
2026-07-08 21:18:40 +0600

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import logoImg from "./assets/logo.png";
+import dollarImg from "./assets/Currency.svg"
+
+
+
+
+function App() {
+  return (
+    <>
+      <div className="navbar primary-font bg-white max-w-300 mx-auto px-4 py-2 flex items-center justify-between">
+        <div className="flex-1">
+          <img src={logoImg} alt="Logo" />
+        </div>
+        <div className="flex gap-2 items-center border border-gray-300 rounded-lg px-4 py-2">
+          <span>6000000000</span> <span>Coin</span>  <img src={dollarImg} alt="Dollar" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
