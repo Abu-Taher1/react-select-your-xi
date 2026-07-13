ece22c4f37bef05092b172d82cddf69b9c820164
Abu-Ther <rafi.xq.1i@gmail.com>
2026-07-13 19:23:31 +0600

    added toggle function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Suspense } from "react";
+import { useState } from "react";
 import Navbar from "./component/Navbar/Navbar.jsx";
 import AvailablePlayer from "./component/AvailablePlayer/AvailablePlayer.jsx";
+import SelectedPlayer from "./component/SelectedPlayer/SelectedPlayer.jsx";
 
 
 
@@ -11,15 +13,32 @@ const fetchPlayers = async () => {
 
 
 function App() {
-
+  const [toggle, setToggle] = useState(true);
   const playersPromise = fetchPlayers();
+
   return (
     <>
       <Navbar />
 
-      <Suspense fallback={<span className="loading loading-dots loading-lg"></span>}>
-        <AvailablePlayer playersPromise={playersPromise}> </AvailablePlayer>
-      </Suspense>
+      <div className="primary-font max-w-300 mx-auto m-10 flex justify-between">
+        <h1 className="text-2xl font-bold text-[#131313]">Available Players</h1>
+         
+        <div className="flex font-bold">
+          <button onClick={() => setToggle(true)} className={`text-[#131313] border border-gray-300 py-3 px-4  rounded-l-2xl border-r-0 ${ toggle === true?"bg-[#E7FE29]":"" }`}>Available</button>
+          <button onClick={() => setToggle(false)} className={`"text-gray-500 border border-gray-300 py-3 px-4 rounded-r-2xl border-l-0 ${toggle === false?"bg-[#E7FE29]":""}`}>Selected <span>(0)</span></button>
+        </div>
+  
+      </div>
+
+      {
+        toggle === true?<Suspense fallback={<span className="loading loading-dots loading-lg"></span>}>
+         <AvailablePlayer playersPromise={playersPromise}> </AvailablePlayer>
+         </Suspense>:<SelectedPlayer></SelectedPlayer>
+      }
+
+      
+
+      
 
 
     </>
---
+++ src/component/SelectedPlayer/SelectedPlayer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedPlayer = () => {
+    return (
+        <div className='primary-font max-w-300 mx-auto m-10'>
+            <p>Selected Player</p>
+        </div>
+    );
+};
+
+export default SelectedPlayer;
